b38071b74a3638e604cb95280ae4732356dd271c
Mira <marina.miroslavska@gmail.com>
2019-02-06 17:51:00 +0200

    Saving autogenerated services ids was added and minor fix with config saving.

--- Services/ConfigService.js
+++ Services/ConfigService.js
@@ -25,7 +25,7 @@ function ConfigService(params) {
         return this.store;
     };
 
-    this.SaveConfig = function (serviceId, data, restartService) {
+    this.SaveConfig = function (serviceName, data, restartService) {
         this.store[serviceName] = data;
         var promise = this.Save(data);
         if (restartService) {
@@ -79,7 +79,23 @@ function ConfigService(params) {
     }).catch(function (err) {
         console.error("ConfigService: Can't get service " + self.filesServiceId);
     });
-};
+
+    ServicesManager.GetServicesInfo().then((services)=>{
+        services.forEach((service)=> {
+            let serviceName;
+            if (service.serviceType === 'ServicesManager'){
+                serviceName = service.serviceType
+            }else{
+                serviceName = service.path;
+            }
+            let serviceId = service.resultId;
+            if (self.store[serviceName] && self.store[serviceName].id !== serviceId){
+                self.store[serviceName].id = serviceId;
+                self.SaveConfig(serviceName, self.store[serviceName]).then(()=>{}).catch(()=>{})
+            }
+        });
+    }).catch(()=>{});
+}
 
 global.serviceId = "ConfigService";
 
