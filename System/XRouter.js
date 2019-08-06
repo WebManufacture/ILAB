dba12e5f65f393dc2e38b6125c2023cadb121812
azzzooo@gmail.com <azzzooo@gmail.com>
2019-08-06 19:03:24 +0300

    XRouter 1st version! (Only Support ids and types)

--- System/XRouter.js
+++ System/XRouter.js
@@ -59,9 +59,121 @@ Node may have another fields:
 //Аспекты передачи сообщений
 
 function XRouter() {
-    this.handlers = [];
-    this.selectors = [];
+    this.structure = {
+        "*": {
+            //there is will be utilization handlers for messages which have additional paths, but not equals any criterias
+            undefined: [
+                //utilization handlers
+            ]
+        },
+        "#id": {
+            undefined: [
+                //end handlers
+            ]
+        },
+        "CALL#12312213": {
+            undefined: [
+                //match handlers
+            ]
+        },
+        undefined: [
+            //end handlers
+        ]
+    };
 }
+
+
+Inherit(XRouter, {
+    on: function (selector, handler) {
+        selector = new Selector(selector);
+        if (selector.isRoot){
+            //Имеет ли это значение тут
+        }
+        return this._subscribe(selector, this.structure, handler);
+    },
+
+    _subscribe(selector, route, handler){
+        var sel = undefined;
+        if (!selector){
+            if (!route[undefined]) route[undefined] = [];
+            route[undefined].push(handler);
+            return route;
+        }
+        if (selector.id){
+            if (selector.type){
+                sel = selector.type + "#" + selector.id;
+
+            } else {
+                sel = "#" + selector.id;
+            }
+            if (!route[sel]) route[sel] = { undefined : [] };
+            return this._subscribe(selector.next, route[sel], handler);
+        } else {
+            if (selector.type){
+                sel = selector.type;
+                if (!route[sel]) route[sel] = { undefined : [] };
+                return this._subscribe(selector.next, route[sel], handler);
+            }
+        }
+        if (!route[undefined]) route[undefined] = [];
+        route[undefined].push(handler);
+        return route;
+    },
+
+    un: function (selectorOrHandler) {
+        return this;
+    },
+
+    route: function (message){
+        var selector = new Selector(message.to);
+        if (selector.isRoot){
+            if (!selector.type && !selector.id){
+                return this._routeInternal(null, this.structure, message);
+            }
+        }
+        return this._routeInternal(selector, this.structure, message);
+    },
+
+    _routeInternal: function (selector, route, message){
+        var sel = '';
+        if (selector){
+            if (selector.id){
+                if (selector.type){
+                    sel = selector.type + "#" + selector.id;
+                    if (route[sel]){
+                        return this._routeInternal(selector.next, route[sel], message);
+                    }
+                }
+                sel = "#" + selector.id;
+                if (route[sel]){
+                    return this._routeInternal(selector.next, route[sel], message);
+                }
+            } else {
+                if (selector.type){
+                    sel = selector.type;
+                    if (route[sel]){
+                        return this._routeInternal(selector.next, route[sel], message);
+                    }
+                }
+            }
+            if (route["*"]){
+                return this._routeInternal(selector.next, route["*"], message);
+            }
+        }
+        return this._callHandlers(message, route, selector);
+    },
+
+    _callHandlers: function (message, route, selector){
+        for (var callback of route[undefined]){
+            var result = callback(message, selector, route);
+        }
+        return true;
+    },
+
+    send: function (selector, data) {
+        return this.route(new RoutingMessage(new Selector(this), selector, data));
+    }
+});
 
 XRouter.TYPE_HI = "hi";        //used when new node up
 XRouter.TYPE_LOOKUP = "lookup";    //used for node search
@@ -69,6 +181,7 @@ XRouter.TYPE_SEEYOU = "seeyou";    //response to lookup packet, or to hi packet
 XRouter.TYPE_BYE = "bye";       //used when node down
 XRouter.TYPE_REDIRECT = "redirect";  //used for subscriptions mechanism, will redirect the path to node
 XRouter.TYPE_FOLLOW = "follow";    //used for subscriptions mechanism, will translate the path to node
+XRouter.TYPE_UNSCRIBE = "unscribe";    //used for subscriptions mechanism, will translate the path to node
 
 XRouter.TYPE_GET_TUNNEL = "get-tunnel";//used for streaming, or direct containers communication
 XRouter.TYPE_AUTO = "auto";      //used for auto-routing
@@ -90,36 +203,21 @@ XRouter.TYPE_TRACE = "trace";     //used for QOS
 XRouter.TYPE_NOTFOUND = "notfound";  //used for QOS
 XRouter.TYPE_CLOSED = "closed";    //used for QOS
 
-RoutingMessage = function (from, to, data) {
+RoutingMessage = function (type, from, to, data) {
     this.id = (Math.random() + "").replace("0.", "");
     this.data = data;
     this.source = {
         from: from,
-        to : to
+        to : to,
+        type : type
     }
-    this.from = Selector.Parse(from);
-    this.to = Selector.Parse(to);
+    this.type = type;
+    this.selector = new Selector(type);
+    if (this.selector.id) this.id = this.selector.id;
+    if (this.selector.type) this.type = this.selector.type;
+    this.from = new Selector(from);
+    this.to = new Selector(to);
     this._creationTime = new Date();
 };
-
-Inherit(XRouter, {
-    on: function (selector, handler) {
-        return this;
-    },
-
-    un: function (selectorOrHandler) {
-        return this;
-    },
-
-    route: function (message){
-
-        return this;
-    },
-
-    send: function (selector, data) {
-        return this.route(new RoutingMessage(new Selector(this), selector, data));
-    }
-});
-
 
 module.exports = XRouter;
